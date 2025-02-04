a9345a3db2bfc1892b0911fead75051a2b9f6497
techdada <22725849+techdada@users.noreply.github.com>
2025-02-04 11:53:32 +0100

    Update run.sh

--- heytech_mqtt_bridge/run.sh
+++ heytech_mqtt_bridge/run.sh
@@ -1,14 +1,30 @@
-#!/usr/bin/env bash
-set -e
+#!/usr/bin/with-contenv bashio
+set +u
 
-echo "Starting Heytech MQTT Bridge..."
+export heyhost=$(bashio::config 'heyhost')
+bashio::log.info "Heytech host: ${heyhost}."
+export heyport=$(bashio::config 'heyport')
+bashio::log.info "Heytech port: ${heyport}."
+export heypin=$(bashio::config 'heypin')
+export mqtthost=$(bashio::config 'mqtthost')
+bashio::log.info "MQTT host: ${mqtthost}."
+export mqttport=$(bashio::config 'mqttport')
+bashio::log.info "MQTT port: ${mqttport}."
+export mqttuser=$(bashio::config 'mqttuser')
+bashio::log.info "MQTT user ${mqttuser}."
+export mqttpass=$(bashio::config 'mqttpass')
+export control_topic_root=$(bashio::config 'control_topic_root')
+bashio::log.info "Control topic: ${control_topic_root}."
+export state_topic_root=$(bashio::config 'state_topic_root')
+bashio::log.info "State topic: ${state_topic_root}."
+bashio::log.info "Starting Heytech MQTT Bridge..."
 
 # Set the config directory for the Node.js application
 export NODE_CONFIG_DIR=/config
 
 # Ensure the config directory exists
 if [ ! -d "$NODE_CONFIG_DIR" ]; then
-  echo "Error: Config directory $NODE_CONFIG_DIR not found!"
+  bashio::log.info "Error: Config directory $NODE_CONFIG_DIR not found!"
   exit 1
 fi
 
